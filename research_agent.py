639d6ba0573d6a3fa7f77c9aeef70293e4503bba
haiderhk <haiderkhan.1050@gmail.com>
2024-12-23 11:42:07 +0500

    Ran for a single analyst

--- research_agent.py
+++ research_agent.py
@@ -1,5 +1,9 @@
+from utils import save_markdown
+from interview_graph import interview_graph
 from create_analysts_graph import create_analysts_graph
+
 from IPython.display import Markdown
+from langchain_core.messages import HumanMessage
 
 
 max_analysts = 3
@@ -31,8 +35,8 @@ if human_analyst_feedback.lower() != "no":
             # for analyst in analysts:
                 # print(analyst.persona)
                 # print("------------------------------------------")
-else:
-    create_analysts_graph.invoke(None, thread)
+# else:
+#     create_analysts_graph.invoke(None, thread)
 
 final_state = create_analysts_graph.get_state(thread)
 analysts = final_state.values.get("analysts")
@@ -42,4 +46,16 @@ for analyst in analysts:
     print(analyst.persona)
 
 
+conduct_interview_message = HumanMessage(f"So you said you were writing an article on {topic}?")
+messages = [conduct_interview_message]
+
+
+#Checking the interview for a single analyst
+interview = interview_graph.invoke({"analyst": analysts[0], "messages": messages, "max_num_turns": 2}, thread)
+
+generated_report_section = interview['sections'][0]
+
+save_markdown(generated_report_section)
+
+print(f"Finished interview with the section: \n\n{generated_report_section}")
 
