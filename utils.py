0f2ad96716245b67eb3b74b0d1e8d1f59f9bdbf2
haiderhk <haiderkhan.1050@gmail.com>
2024-12-23 10:30:43 +0500

    Moved conditions and added a messages route condition for saving/continuing.

--- utils.py
+++ utils.py
@@ -1,7 +1,42 @@
 import base64
 from pathlib import Path
+from langgraph.graph import END
+from langchain_core.messages import AIMessage
+
+from schemas import GenerateAnalystsState, InterviewState
+
 
 def save_image(graph):
     with open("assets/human-in-the-loop.png", "wb") as fout:
         fout.write(graph.get_graph().draw_mermaid_png())
         print("Graph Image saved!")
+
+
+def should_continue_condition(state: GenerateAnalystsState):
+    """Return the next node to execute based on human feedback"""
+    human_analyst_feedback = state.get("human_analyst_feedback", None)
+
+    if human_analyst_feedback:
+        return "create_analysts"
+    else:
+        END
+
+
+def route_messages_condition(state: InterviewState, name: str = "expert"):
+    messages = state["messsages"]
+    max_num_turns = state.get("max_num_turns", 2)
+
+    num_responses = 0
+    for message in messages:
+        if isinstance(message, AIMessage) and message.name == name:
+            num_responses += 1
+
+    if num_responses >= max_num_turns:
+        return "Save Interview"
+    
+    last_question = messages[-2]
+    
+    if "Thank you for your help" in last_question.content:
+        return "Save Interview"
+    
+    return "Ask Question"
