e3087cec4d084c281a4d792bc4691d4f79f96df8
haiderhk <haiderkhan.1050@gmail.com>
2024-12-23 11:41:27 +0500

    A better model for tool use

--- initialize_groq.py
+++ initialize_groq.py
@@ -5,5 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-llm = ChatGroq(model = "llama3-70b-8192", temperature=0)
+# llm = ChatGroq(model = "llama3-70b-8192", temperature=0)
+llm = ChatGroq(model = "llama3-groq-70b-8192-tool-use-preview", temperature=0)
 
